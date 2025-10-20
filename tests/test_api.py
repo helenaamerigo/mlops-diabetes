a7e0fe81d6655ee6934b4018b51e891b760a59e7
helenaamerigo <hameriar42@alumnes.ub.edu>
2025-10-20 19:21:04 +0200

    test p2

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,29 +1,18 @@
-# tests/test_api.py
-import requests
+from fastapi.testclient import TestClient
+from app.main import app  
+client = TestClient(app)
 
 def test_health():
-    # Suponiendo que tu contenedor esté corriendo localmente en el puerto 8000
-    response = requests.get("http://127.0.0.1:8000/health")
+    response = client.get("/health")
     assert response.status_code == 200
-    json_data = response.json()
-    assert "status" in json_data
-    assert "model_version" in json_data
+    assert response.json()["status"] == "ok"
 
 def test_predict():
     payload = {
-        "age": 0.02,
-        "sex": -0.044,
-        "bmi": 0.06,
-        "bp": -0.03,
-        "s1": -0.02,
-        "s2": 0.03,
-        "s3": -0.02,
-        "s4": 0.02,
-        "s5": 0.02,
-        "s6": -0.001
+        "age": 0.02, "sex": -0.044, "bmi": 0.06, "bp": -0.03,
+        "s1": -0.02, "s2": 0.03, "s3": -0.02, "s4": 0.02,
+        "s5": 0.02, "s6": -0.001
     }
-    response = requests.post("http://127.0.0.1:8000/predict", json=payload)
+    response = client.post("/predict", json=payload)
     assert response.status_code == 200
-    json_data = response.json()
-    assert "prediction" in json_data
-    assert isinstance(json_data["prediction"], (float, int))
+    assert "prediction" in response.json()
